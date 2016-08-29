310900145465ef44082c04a3b72a7982908d625b
Boris Yen <yulinyen@gmail.com>
2016-08-29 17:16:22 +0800

    daily commit

--- dbconnection.js
+++ dbconnection.js
@@ -12,6 +12,10 @@ var sequelize = new Sequelize('stock', 'byan', 'byan', {
     }
 }) ;
 
+/**
+ * id: 證券代號
+ * name: 證券名稱
+ */
 exports.Stock = sequelize.define('stock', {
     id: { 
         type: Sequelize.STRING(15),
@@ -26,6 +30,21 @@ exports.Stock = sequelize.define('stock', {
     collate: 'utf8_general_ci'
 }) ;
 
+/**
+ * id: 證券代號
+ * date: 日期
+ * vol: 成交股數
+ * turnover: 成交金額
+ * open: 開盤價
+ * high: 最高價
+ * low: 最低價
+ * close: 收盤價
+ * diff: 漲跌價差
+ * transations: 成交筆數
+ * pb_ratio: 股價淨值比
+ * pe_ratio: 本益比
+ * yields: 殖利率
+ */
 exports.StockDailyInfo = sequelize.define('stock_daily_info', {
     id: {
         type: Sequelize.STRING(15),
